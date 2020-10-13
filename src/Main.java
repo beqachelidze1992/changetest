ab42e878de1e70f124946e5553d26f5bb66c8bfc
beqachelidze1992 <beqachelidzegit@gmail.com>
2020-10-13 20:15:37 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Git test");
+        System.out.println("Git test2");
+        System.out.println("Git test3");
+        System.out.println("Git test4");
+
+        System.out.println("asdlkjfdsrhvwebktrhe");
+        System.out.println("asdlkjfdsrhvwebktrhe");
+        System.out.println("asdlkjfdsrhvwebktrhe");
+
+
+        System.out.println("test +-");
+
+
+    }
+}
---
+++ src/Person2.java
@@ -0,0 +1,13 @@
+public class Person2 {
+
+    public void f(){
+
+        System.out.println("test");
+    }
+
+    public void f2(){
+
+        System.out.println("test2");
+    }
+
+}
